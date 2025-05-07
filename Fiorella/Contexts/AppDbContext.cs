bde3099d41cbb38fa91fecc48a03faf909a99c15
ilkinmammad <ilkinmammadli04@gmail.com>
2025-05-07 19:39:23 +0400

    Homework

---
+++ Fiorella/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Fiorella.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorella.Contexts;
+
+public class AppDbContext : DbContext
+{
+    private readonly string _connectionString = @"Server=CODE_PC_7_213_1\SQLEXPRESS;Database=FiorellaDb;Trusted_Connection=True;TrustServerCertificate=True";
+
+    public DbSet<Flower> Flower { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ Fiorella/Models/Flower.cs
@@ -0,0 +1,11 @@
+﻿namespace Fiorella.Models;
+
+public class Flower
+{
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImgUrl { get; set; }
+
+}
