2506184f3cbe89e8029f18e811e7cbf74cf14b34
Ahmedova88 <gunayfa-ap216@code.edu.az>
2025-05-16 11:40:52 +0400

    Gym updated

---
+++ GymMVC/GYMProject/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GYMProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GYMProject.Contexts;
+
+public class AppDbContext : DbContext
+{
+    private readonly string _connectionString = @"Server=BRILYANT\SQLEXPRESS;Database=GymDb2;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    public DbSet<GymProgram> gymPrograms { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
